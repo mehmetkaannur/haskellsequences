b5fddb9df94517bf8a76ae6d56a756a43e1e60b3
Mehmet Nur <mkn123@ash06.doc.ic.ac.uk>
2023-10-06 15:25:49 +0100

    Implementation of utlity functions

--- Sequences.hs
+++ Sequences.hs
@@ -5,34 +5,44 @@ import Data.Char (ord, chr)
 -- | Returns the first argument if it is larger than the second,
 -- the second argument otherwise
 maxOf2 :: Int -> Int -> Int
-maxOf2 = undefined
+maxOf2 x y
+  | x > y = x
+  | otherwise = y
 
 -- | Returns the largest of three Ints
 maxOf3 :: Int -> Int -> Int -> Int
-maxOf3 = undefined
+maxOf3 x y z = maxOf2 (maxOf2 x y) z
+
+-- maxOf3 :: Int -> Int -> Int -> Int
+-- maxOf3 x y z
+--   | x >= y && x >= z = x
+--   | y >= x && y >= z = y
+--   | z >= y && z >= x = z
 
 -- | Returns True if the character represents a digit '0'..'9';
 -- False otherwise
 isADigit :: Char -> Bool
-isADigit = undefined
+isADigit x = ord(x) >= 48 && ord(x) <= 57
 
 -- | Returns True if the character represents an alphabetic
 -- character either in the range 'a'..'z' or in the range 'A'..'Z';
 -- False otherwise
 isAlpha :: Char -> Bool
-isAlpha = undefined
+isAlpha x = (ord(x) >= 65 && ord(x) <= 90) || (ord(x) >= 97 && ord(x) <= 122)
 
 -- | Returns the integer [0..9] corresponding to the given character.
 -- Note: this is a simpler version of digitToInt in module Data.Char,
 -- which does not assume the precondition.
 digitToInt :: Char -> Int
 -- Pre: the character is one of '0'..'9'
-digitToInt = undefined
+digitToInt x = ord(x) - 48
 
 -- | Returns the upper case character corresponding to the input.
 -- Uses guards by way of variety.
 toUpper :: Char -> Char
-toUpper = undefined
+toUpper x
+  | ord(x)>=97 && ord(x)<=122 = chr(ord(x) - 32)
+  | otherwise = x
 
 --
 -- Sequences and series
